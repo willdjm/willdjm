48ee7d9acb811c8f7ecb115730ef2c628d72c3ff
willdjm <will_djm@hotmail.com>
2023-10-31 00:37:50 -0300

    first commit

---
+++ src/app/components/NavBar.tsx
@@ -0,0 +1,60 @@
+import Image from 'next/image';
+import Linkk from 'next/link';
+import { useState } from 'react';
+import { Link } from 'react-scroll';
+import { BiMenu } from 'react-icons/bi';
+import { IoCloseSharp } from 'react-icons/io5';
+
+
+export function NavBar() {
+  const [navbar, setNavbar] = useState(false);
+  return (
+    <div id='Top'>
+      <nav className="w-full fixed top-10 left-0 right-0 z-10">
+        <div className="justify-between mx-auto md:items-center md:flex md:px-8">
+          <div>
+            <div className="flex items-center justify-end  md:block">
+              {/* LOGO */}
+
+              {/* HAMBURGER BUTTON FOR MOBILE */}
+              <div className="md:hidden px-4">
+                <button
+                  className=" text-gray-700 rounded-md outline-none"
+                  onClick={() => setNavbar(!navbar)}
+                >
+                  {navbar ? (
+                    <IoCloseSharp size={30} className='text-white'/>
+                      ) : (
+                    <BiMenu size={30} className='text-white'/>
+                  )}
+                </button>
+              </div>
+            </div>
+          </div>
+          <div>
+            <div
+              className={`w-full h-full flex items-center justify-center md:block md:pb-0 md:mt-0 ${
+                navbar ? 'p-0 md:p-0 block bg-black opacity-80 md:bg-transparent w-full' : 'hidden'
+              }`}
+            >
+              <div id='home' className="h-screen md:h-auto md:items-end items-center justify-center md:grid gap-5">
+                <ul className='md:flex md:items-center md:justify-end gap-10'>
+                <li className="text-base text-white py-2 px-6 md:py-0 md:px-0 text-center border-b-2 md:border-b-2 md:border-opacity-0 border-white md:hover:bg-transparent">
+                <Linkk className='cursor-pointer' href="/" onClick={() => setNavbar(!navbar)}>Portifolio</Linkk>
+                </li>
+                <li className="text-base  text-white py-2 px-6 md:py-0 md:px-0 text-center border-b-2 md:border-b-2 md:border-opacity-0 border-white md:hover:bg-transparent">
+                <Linkk className='cursor-pointer' href="/page-history" onClick={() => setNavbar(!navbar)}>About</Linkk>
+                </li>
+                <li className="text-base  text-white py-2 px-6 md:py-0 md:px-0 text-center border-b-2 md:border-b-2 md:border-opacity-0 border-white md:hover:bg-transparent">
+                <Link className='cursor-pointer' to="Contact" spy={true} smooth={true} offset={-100} duration={500} delay={100} onClick={() => setNavbar(!navbar)}>Contato</Link>
+                </li>
+
+                </ul>
+              </div>
+            </div>
+          </div>
+        </div>
+      </nav>
+    </div>
+  );
+}
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,15 +2,17 @@
 
 import { AiFillGithub, AiOutlineInstagram } from 'react-icons/ai';
 import { MdOutlineMailOutline } from 'react-icons/md';
-import { BiLogoFacebook } from 'react-icons/bi';
-import { BsLinkedin } from 'react-icons/bs';
+import { FaFacebookF } from 'react-icons/fa';
+import { BsLinkedin, BsWhatsapp } from 'react-icons/bs';
 
 import {ParticlesComponent} from './components/Particles';
 import Link from 'next/link';
+import { NavBar } from './components/NavBar';
 
 export default function Home() {
   return (
 <main className='h-screen bg-black opacity-90 flex flex-col items-center justify-center'>
+  <NavBar/>
 <ParticlesComponent id="tsparticles" />
 <div className='flex flex-col gap-2 relative'>
 <Link href="https://github.com/willdjm" >
@@ -31,16 +33,21 @@ className='flex gap-1 items-center cursor-pointer text-base lg:justify-end'>
 </Link>
 <Link href="https://www.instagram.com/willdjm/"
 className='flex gap-1 items-center cursor-pointer text-base lg:justify-end'>
-<AiOutlineInstagram size={30} />
+<AiOutlineInstagram size={33} />
 </Link>
 <Link href="https://www.facebook.com.br/wilkinsondouglas/"
 className='flex gap-1 items-center cursor-pointer text-base lg:justify-end'>
-<BiLogoFacebook size={30} />
+<FaFacebookF size={30} />
+</Link>
+<Link href="mailto:wilkindondjm@gmail.com"
+className='flex items-center cursor-pointer text-base lg:justify-end'>
+<BsWhatsapp size={30} />
 </Link>
 <Link href="mailto:wilkindondjm@gmail.com"
 className='flex items-center cursor-pointer text-base lg:justify-end'>
 <MdOutlineMailOutline size={30} />
 </Link>
+
 
 </div>
 </div>
