8bc2513577aa2df07895a4b10d5da0dd2a1a6542
willdjm <will_djm@hotmail.com>
2023-11-02 13:36:39 -0300

    init

--- src/app/components/NavBar.tsx
+++ src/app/components/NavBar.tsx
@@ -60,8 +60,6 @@ export function NavBar() {
                 <li className="text-base  text-white py-2 px-6 md:py-0 md:px-0 text-center border-b-2 md:border-b-2 md:border-opacity-0 border-white md:hover:bg-transparent">
                 <Linkk className='cursor-pointer' href="#tec" onClick={() => setNavbar(!navbar)}>CONTACT</Linkk>
                 </li>
-
-
                 </ul>
               </div>
             </div>
