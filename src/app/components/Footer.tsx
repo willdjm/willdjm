4b0563a8f1d5542225ac0ff3de51e4d8d67a64f2
willdjm <will_djm@hotmail.com>
2023-11-02 14:00:48 -0300

    init

---
+++ src/app/components/Footer.tsx
@@ -0,0 +1,11 @@
+export function Footer() {
+
+return (
+
+<footer className="bg-slate-900 py-5 flex items-center justify-center">
+                <h1 className='col-span-2 text-xs font-light text-center text-white uppercase'>© Wdjm Corp, .</h1>
+    </footer>
+)
+}
+
+
