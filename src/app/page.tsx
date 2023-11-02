4bfac2ab257e61c63a40072bc482880633106a2b
willdjm <will_djm@hotmail.com>
2023-11-02 13:58:50 -0300

    init

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -12,7 +12,6 @@ import { WDJMCorp } from './components/WDJMCorp';
 import { AboutMe } from './components/About';
 import { Portifolio } from './components/Portifolio';
 import { Tec } from './components/Tec';
-import { Footer } from './components/Footer';
 
 export default function Home() {
   return (
@@ -23,7 +22,7 @@ export default function Home() {
 <AboutMe/>
 <Portifolio/>
 <Tec/>
-<Footer/>
+
 
 </main>
   )
